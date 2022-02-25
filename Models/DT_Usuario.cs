943898e55dde190e5b5673f1a12f07ab2ca8e457
CarlosDavid07 <cdrodriguezc@unbosque.edu.co>
2022-02-25 12:15:02 -0500

    Se eliminan Campos no funcionales
    CT_Empresa
    DR_Usuario

--- Models/DT_Usuario.cs
+++ Models/DT_Usuario.cs
@@ -36,20 +36,6 @@ namespace DynamicForecast.Models
 
         public int TipoUsuario { get; set; }
 
-        public int? CargoId { get; set; }
-
-        public int? NivelUsr { get; set; }
-
-        [StringLength(4)]
-        public string CalendarId { get; set; }
-
-        [StringLength(1)]
-        public string Capacidad { get; set; }
-
-        public int? MenuId { get; set; }
-
-        public int? FotoId { get; set; }
-
         [Required]
         [StringLength(2)]
         public string Estado { get; set; }
@@ -58,85 +44,14 @@ namespace DynamicForecast.Models
         [StringLength(3)]
         public string CausaEstado { get; set; }
 
-        public int UsuarioAct { get; set; }
-
         public DateTime FechaIng { get; set; }
 
         public DateTime FechaMod { get; set; }
 
-        public int? CiudadId { get; set; }
 
-        [StringLength(120)]
-        public string Direccion { get; set; }
-
-        [StringLength(30)]
-        public string Telefono { get; set; }
-
-        [StringLength(30)]
-        public string Celular { get; set; }
-
-        [StringLength(30)]
-        public string Fax { get; set; }
-
-        public int? FirmaId { get; set; }
-
-        [StringLength(1)]
-        public string TodosProg { get; set; }
-
-        [StringLength(1)]
-        public string AccesoPC { get; set; }
-
-        public DateTime? FechaVig { get; set; }
-
-        public int? MapaProcId { get; set; }
-
-        public int? PadreId { get; set; }
-
-        public int? Autenticacion { get; set; }
-
-        [StringLength(20)]
-        public string DatoTec1 { get; set; }
-
-        [StringLength(20)]
-        public string DatoTec2 { get; set; }
-
-        [StringLength(50)]
-        public string DatoTec3 { get; set; }
-
-        [StringLength(50)]
-        public string DatoTec4 { get; set; }
-
-        [StringLength(50)]
-        public string DatoTec5 { get; set; }
-
-        [StringLength(30)]
-        public string CodigoAlt { get; set; }
-
-        [StringLength(30)]
-        public string TarjetaNro { get; set; }
-
-        [StringLength(1)]
-        public string PerfilId { get; set; }
-
-        public int? EspecialId { get; set; }
-
-        [StringLength(20)]
-        public string Nombre1 { get; set; }
-
-        [StringLength(20)]
-        public string Nombre2 { get; set; }
-
-        [StringLength(20)]
-        public string Apellido1 { get; set; }
-
-        [StringLength(20)]
-        public string Apellido2 { get; set; }
 
         [StringLength(1)]
         public string UsuarioAdmin { get; set; }
-
-        [StringLength(1)]
-        public string IndDashboardPropio { get; set; }
 
 
         public string PalabraClaveActual { get; set; }
